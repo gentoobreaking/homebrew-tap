b0655ab9e477b25ab59a2d08b713b2ddc7f6ae76
Yuhao Chen <gentoobreaking@gmail.com>
2026-06-15 01:41:02 +0800

    backup

---
+++ Casks/md-viewer.rb
@@ -0,0 +1,23 @@
+cask "md-viewer" do
+  version "0.2.2"
+  sha256 :no_check
+
+  url "https://github.com/gentoobreaking/md-viewer/releases/download/v#{version}/md-viewer-#{version}.dmg"
+
+  name "md-viewer"
+  desc "Markdown 文件檢視器"
+  homepage "https://github.com/gentoobreaking/md-viewer"
+
+  app "md-viewer.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/md-viewer.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/md-viewer",
+    "~/Library/Preferences/md-viewer.plist",
+  ]
+end
